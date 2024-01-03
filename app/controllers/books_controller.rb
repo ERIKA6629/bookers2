c970c6def2f709c018f8f22e8d345bd1433f380c
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-01-03 13:55:24 +0000

    [add]book-createは失敗

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,28 @@ class BooksController < ApplicationController
   end
   
   def new
-    
+    @book = Book.new
+  end
+ 
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    byebug
+    redirect_to book_path(book)
   end
   
   def index
+   
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
     
   end
   
