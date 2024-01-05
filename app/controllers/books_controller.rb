07121a36f989361e2930269312a3ec4d8c6bba38
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-01-05 14:28:08 +0000

    [add]bookの編集、削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,13 @@
 class BooksController < ApplicationController
   
   def edit
-    
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   def new
@@ -14,6 +20,12 @@ class BooksController < ApplicationController
     book.user_id = current_user.id
     book.save!
     redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.delete
+    redirect_to books_path
   end
   
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, :update]
-  resources :users, only: [:edit , :index, :show, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:edit , :index, :show, :update, :destroy]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'homes/about' => 'homes#about', as: 'about'
